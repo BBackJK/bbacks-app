3b08285e2a61ffe7de10562ecac392817854aae8
bbacks <wnsrud6839@gmail.com>
2021-05-19 17:36:57 +0900

    feat: input component

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ButtonComponent } from './button/button.component';
 import { LinkComponent } from './link/link.component';
+import { InputCheckboxComponent, InputRadioComponent, InputTextComponent } from './input';
+
 
 @NgModule({
-  imports: [ CommonModule ],
+  imports: [ CommonModule, ReactiveFormsModule ],
   exports: [
     ButtonComponent
     , LinkComponent
+    , InputCheckboxComponent
+    , InputRadioComponent
+    , InputTextComponent
   ],
   declarations: [
     ButtonComponent
     , LinkComponent
+    , InputCheckboxComponent
+    , InputRadioComponent
+    , InputTextComponent
   ]
 })
 export class ComponentsModule {}
