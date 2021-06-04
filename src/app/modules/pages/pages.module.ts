c8bba3020c6878e9eeaac30c2556f0d4f8334f80
bbacks <wnsrud6839@gmail.com>
2021-06-04 17:47:28 +0900

    feat/add modules pages of calendar, diary

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { HomeModule } from './home/home.module';
+import { CalendarModule } from './calendar/calendar.module';
+import { DiaryModule } from './diary/diary.module';
 
 @NgModule({
   imports: [],
-  exports: [ HomeModule ],
+  exports: [ HomeModule, CalendarModule, DiaryModule ],
   declarations: []
 })
 export class PagesModule {}
