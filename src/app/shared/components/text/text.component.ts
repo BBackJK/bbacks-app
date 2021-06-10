6f52e3947fa28dd7b67f5b21d52c18ea9c27dae3
bbacks <wnsrud6839@gmail.com>
2021-06-10 16:46:59 +0900

    feat: modify shared component text

--- src/app/shared/components/text/text.component.ts
+++ src/app/shared/components/text/text.component.ts
@@ -7,32 +7,26 @@ import { Component, Input, OnInit, OnChanges } from '@angular/core';
 })
 export class TextComponent implements OnInit, OnChanges {
 
-  @Input() type: string;
-  @Input() text: string;
-
+  @Input() type: string = 'inline';
   @Input() textSize: string;
   @Input() textColor: string;
 
   textSizeClassName: string;
   textColorClassName: string;
+  displayClassName: string;
   
   constructor() {}
 
   ngOnChanges(): void {
-    this.setDefaultTextType();
     this.setClassName();
   }
   ngOnInit(): void {
-    this.setDefaultTextType();
     this.setClassName();
-  }
-
-  setDefaultTextType(): void {
-    this.type = this.type ? this.type : 'inline'
   }
 
   setClassName(): void {
     this.textSizeClassName = this.textSize ? 'text-size-' + this.textSize : 'text-size-md';
     this.textColorClassName = this.textColor ? 'text-color-' + this.textColor : 'text-color-dark';
+    this.displayClassName = this.type ? 'text-display-' + this.type : 'text-display-inline';
   }
 }
