d2b0c0dd25c8ed8647f1e488e0d659cb029ea401
bbacks <wnsrud6839@gmail.com>
2021-06-17 15:21:54 +0900

    feat: add home component ui

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+import { addZeroString, initCalendarObject } from '@shared/utils';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,26 +10,38 @@ import { FormControl } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-  open: boolean = true;
+  years: number = initCalendarObject.getYears();
+  months: number | string = addZeroString(initCalendarObject.getMonths());
+  dates: number | string = addZeroString(initCalendarObject.getDates());
+
+  days: string = initCalendarObject.dayList[initCalendarObject.getDays()];
+
+  minutes: number | string = addZeroString(initCalendarObject.getMinutes());
+  hours: number | string = addZeroString(initCalendarObject.getHours());
+  seconds: number | string = addZeroString(initCalendarObject.getSeconds());
 
   constructor() {}
 
-  test: FormControl = new FormControl('');
-
   ngOnInit(): void {
-    console.log('home');
+    this.setTime();
   }
 
-  onTest():void {
-    console.log('click');
-    console.log(this.test.value);
-  }
+  setTime(): void {
+    const $this = this;
 
-  closeTest(): void {
-    console.log(1);
-  }
+    setInterval(function() {
 
-  onClick():void {
-    this.open = !this.open;
+      let now = new Date();
+
+      $this.years = initCalendarObject.getYears(now);
+      $this.months = addZeroString(initCalendarObject.getMonths(now));
+      $this.dates = addZeroString(initCalendarObject.getDates(now));
+      $this.days = initCalendarObject.dayList[initCalendarObject.getDays(now)];
+      $this.minutes = addZeroString(initCalendarObject.getMinutes(now));
+      $this.hours = addZeroString(initCalendarObject.getHours(now));
+      $this.seconds = addZeroString(initCalendarObject.getSeconds(now));
+
+      now = null;
+    }, 1000);
   }
 }
